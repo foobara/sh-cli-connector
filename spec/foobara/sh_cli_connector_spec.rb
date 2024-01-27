2e4a28e3e32a304664419ee952aa749c4ac366a1
Miles Georgi <azimux@gmail.com>
2024-01-26 18:56:07 -0800

    Initial commit, generated from RubyMine

---
+++ spec/foobara/sh_cli_connector_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Foobara::ShCliConnector do
+  it "has a version number" do
+    expect(Foobara::ShCliConnector::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
