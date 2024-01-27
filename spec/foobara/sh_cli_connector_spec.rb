3c89f706ab8694f87a0bacb31fbb0821ea578087
Miles Georgi <azimux@gmail.com>
2024-01-26 19:17:41 -0800

    Autocorrect with rubocop

--- spec/foobara/sh_cli_connector_spec.rb
+++ spec/foobara/sh_cli_connector_spec.rb
@@ -1,7 +1,5 @@
-# frozen_string_literal: true
-
 RSpec.describe Foobara::ShCliConnector do
   it "has a version number" do
-    expect(Foobara::ShCliConnector::VERSION).not_to be nil
+    expect(Foobara::ShCliConnector::VERSION).to_not be_nil
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require "foobara/sh_cli_connector"
 
 RSpec.configure do |config|
